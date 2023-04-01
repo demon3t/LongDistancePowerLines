3b9e4746a85876586c3e604530ac69d963a1078d
danil.sadyrin@mail.ru <danil.sadyrin@mail.ru>
2023-04-01 20:16:05 +0300

    Добавил расчёт напряжения больше, меньше натуральной мощности, а так же при одностороннем включении линии.

--- LongDistancePowerLinesCalsulate/Classes/OutputData/VoltageDistribution.cs
+++ LongDistancePowerLinesCalsulate/Classes/OutputData/VoltageDistribution.cs
@@ -20,6 +20,8 @@ namespace LongDistancePowerLinesCalsulate.Classes.OutputData
 
         }
 
+        #region Котангенс
+
         /// <summary>
         /// Котангенс комплексного числа.
         /// </summary>
@@ -30,15 +32,26 @@ namespace LongDistancePowerLinesCalsulate.Classes.OutputData
             return 1 / Tan(x);
         }
 
+        /// <summary>
+        /// Котангенс числа.
+        /// </summary>
+        /// <param name="x">Число.</param>
+        /// <returns>Результат.</returns>
+        private static double Ctg(double x)
+        {
+            return 1 / Math.Tan(x);
+        }
+
+        #endregion
+
         #region Токи
 
         /// <summary>
         /// Ток больше натуральной мощности.
         /// </summary>
-        /// <returns>Значения тока.</returns>
-        private Complex I2_more 
+        private Complex I2_more
         {
-            get 
+            get
             {
                 var P2 = _inputData.K2 * _inputData.NaturalPower;
 
@@ -48,30 +61,190 @@ namespace LongDistancePowerLinesCalsulate.Classes.OutputData
 
                 var Q2 = _Q2.Magnitude;
 
-                var deg = Math.Atan((_Q2 * _inputData.NaturalPower).Magnitude / (_inputData.K2 * _inputData.NaturalPower));
+                var rad = Math.Atan((_Q2 * _inputData.NaturalPower).Magnitude / (_inputData.K2 * _inputData.NaturalPower));
 
                 return new Complex(
-                           Math.Sqrt(Math.Pow(P2, 2) + Math.Pow(Q2, 2)) / _inputData.NominalVoltage * Math.Cos(deg),
-                           Math.Sqrt(Math.Pow(P2, 2) + Math.Pow(Q2, 2)) / _inputData.NominalVoltage * Math.Sin(deg));
+                           Math.Sqrt(Math.Pow(P2, 2) + Math.Pow(Q2, 2)) / _inputData.NominalVoltage * Math.Cos(rad),
+                           Math.Sqrt(Math.Pow(P2, 2) + Math.Pow(Q2, 2)) / _inputData.NominalVoltage * Math.Sin(rad));
+            }
+        }
+
+        /// <summary>
+        /// Ток меньше натуральной мощности.
+        /// </summary>
+        private Complex I2_less
+        {
+            get
+            {
+                var P2 = _inputData.K1 * _inputData.NaturalPower;
+
+                var _Q2 = Ctg(_inputData.PhaseCoefficient * _inputData.LineLength) +
+                    Math.Sqrt(1 / Math.Pow(Math.Sin(_inputData.PhaseCoefficient * _inputData.LineLength), 2) -
+                    Math.Pow(_inputData.K1, 2));
+
+                var Q2 = _Q2 * _inputData.NaturalPower;
+
+                var rad = Math.Atan(Q2 / P2);
+
+                return new Complex(
+                    Math.Sqrt(Math.Pow(P2, 2) + Math.Pow(Q2, 2)) / _inputData.NominalVoltage * Math.Cos(-rad),
+                    Math.Sqrt(Math.Pow(P2, 2) + Math.Pow(Q2, 2)) / _inputData.NominalVoltage * Math.Sin(-rad));
             }
         }
 
         #endregion
 
+        #region Напряжение при натуральной мощности
 
         /// <summary>
-        /// Натуральная мощность.
+        /// Напряжение при натуральной мощности.
         /// </summary>
-        public double GetNaturalPower =>
+        public double GetVoltage_NaturalPower(double l) =>
             _inputData.NominalVoltage;
 
         /// <summary>
-        /// Мощность больше натуральной.
+        /// Получить распределение напряжения вдоль линии при натуральной мощности.
         /// </summary>
-        public double GetPowerMoreNatural(double l) => (Math.Cos(_inputData.PhaseCoefficient * (_inputData.LineLength - l)) *
+        /// <param name="Xs">Расстояний от начала линии.</param>
+        /// <param name="Ys">Значение напряжения.</param>
+        public void GetVoltageCollection_NaturalPower(out double[] Xs, out double[] Ys)
+        {
+            Xs = new double[2];
+            Ys = new double[2];
+
+            Xs[0] = 0;
+            Xs[1] = _inputData.LineLength;
+            Ys[0] = _inputData.NominalVoltage;
+            Ys[1] = _inputData.NominalVoltage;
+        }
+
+        #endregion
+
+        #region Напряжение при мощности больше натуральной
+
+        /// <summary>
+        /// Напряжение при мощности больше натуральной.
+        /// </summary>
+        /// <param name="l">Расстояние от начала линии.</param>
+        /// <returns>Напряжение.</returns>
+        public double GetVoltage_MoreNaturalPower(double l) => (Math.Cos(_inputData.PhaseCoefficient * (_inputData.LineLength - l)) *
             _inputData.NominalVoltage + _inputData.WaveResistanceLine.Magnitude * ImaginaryOne * Math.Sin(_inputData.PhaseCoefficient *
                 (_inputData.LineLength - l)) * I2_more).Magnitude;
 
-        //todo : сделать остальные напряжения
+        /// <summary>
+        /// Получить распределение напряжения вдоль линии при мощности больше натуральной.
+        /// </summary>
+        /// <returns>
+        /// <para>Xs - Расстояний от начала линии.</para>
+        /// <para>Ys - Значение напряжения.</para>
+        /// </returns>
+        public (double[] Xs, double[] Ys) GetVoltageCollection_MoreNaturalPower()
+        {
+            double[] Xs = new double[(int)_inputData.LineLength + 1];
+            double[] Ys = new double[(int)_inputData.LineLength + 1];
+
+            double beta = _inputData.WavePropagationCoefficient.Magnitude * Math.Sin(_inputData.WavePropagationCoefficient.Phase);
+            Complex i2 = I2_more;
+            double z = _inputData.WaveResistanceLine.Magnitude;
+
+            Parallel.For(0, Xs.Length, (i) =>
+            {
+                Xs[i] = i;
+                Ys[i] = (Math.Cos(beta * (_inputData.LineLength - i)) * _inputData.NominalVoltage +
+                z * ImaginaryOne * Math.Sin(beta * (_inputData.LineLength - i)) * i2).Magnitude;
+            });
+
+            return (Xs, Ys);
+        }
+
+
+        #endregion
+
+        #region Напряжение при мощности меньше натуральной
+
+        /// <summary>
+        /// Напряжение при мощности меньше натуральной.
+        /// </summary>
+        /// <param name="l">Расстояние от начала линии.</param>
+        /// <returns>Напряжение.</returns>
+        public double GetVoltage_LessNaturalPower(double l) => (Math.Cos(_inputData.PhaseCoefficient * (_inputData.LineLength - l)) *
+            _inputData.NominalVoltage + _inputData.WaveResistanceLine.Magnitude * ImaginaryOne * Math.Sin(_inputData.PhaseCoefficient *
+                (_inputData.LineLength - l)) * I2_less).Magnitude;
+
+        /// <summary>
+        /// Получить распределение напряжения вдоль линии при мощности меньше натуральной.
+        /// </summary>
+        /// <returns>
+        /// <para>Xs - Расстояний от начала линии.</para>
+        /// <para>Ys - Значение напряжения.</para>
+        /// </returns>
+        public (double[] Xs, double[] Ys) GetVoltageCollection_LessNaturalPower()
+        {
+            double[] Xs = new double[(int)_inputData.LineLength + 1];
+            double[] Ys = new double[(int)_inputData.LineLength + 1];
+
+            double beta = _inputData.WavePropagationCoefficient.Magnitude * Math.Sin(_inputData.WavePropagationCoefficient.Phase);
+            Complex i2 = I2_less;
+            double z = _inputData.WaveResistanceLine.Magnitude;
+
+            Parallel.For(0, Xs.Length, (i) =>
+            {
+                Xs[i] = i;
+                Ys[i] = (Math.Cos(beta * (_inputData.LineLength - i)) * _inputData.NominalVoltage +
+                z * Complex.ImaginaryOne * Math.Sin(beta * (_inputData.LineLength - i)) * i2).Magnitude;
+            });
+
+            return (Xs, Ys);
+        }
+
+        #endregion
+
+        #region Напряжение при одностороннем включении
+
+        /// <summary>
+        /// Напряжение при одностороннем включении.
+        /// </summary>
+        /// <param name="l">Расстояние от начала линии.</param>
+        /// <returns>Напряжение.</returns>
+        public double GetVoltage_OneSided(double l)
+        {
+            var beta = _inputData.WavePropagationCoefficient.Magnitude * Math.Sin(_inputData.WavePropagationCoefficient.Phase);
+            var a = (Complex)Math.Cos(beta * _inputData.LineLength);
+            var b = _inputData.WaveResistanceLine.Magnitude * Math.Sin(beta * _inputData.LineLength) * ImaginaryOne;
+            var i2 = I2_more;
+
+            return Math.Cos(beta * (_inputData.LineLength - l)) *
+                ((a * _inputData.NominalVoltage + b * I2_more).Magnitude / a.Magnitude);
+        }
+
+        /// <summary>
+        /// Получить распределение напряжения вдоль линии при одностороннем включении.
+        /// </summary>
+        /// <returns>
+        /// <para>Xs - Расстояний от начала линии.</para>
+        /// <para>Ys - Значение напряжения.</para>
+        /// </returns>
+        public (double[] Xs, double[] Ys) GetVoltageCollection_OneSided()
+        {
+            double[] Xs = new double[(int)_inputData.LineLength + 1];
+            double[] Ys = new double[(int)_inputData.LineLength + 1];
+
+            var beta = _inputData.WavePropagationCoefficient.Magnitude * Math.Sin(_inputData.WavePropagationCoefficient.Phase);
+            var a = (Complex)Math.Cos(beta * _inputData.LineLength);
+            var b = _inputData.WaveResistanceLine.Magnitude * Math.Sin(beta * _inputData.LineLength) * ImaginaryOne;
+            var i2 = I2_more;
+
+            Parallel.For(0, Xs.Length, (i) =>
+            {
+                Xs[i] = i;
+                Ys[i] = Math.Cos(beta * (_inputData.LineLength - i)) *
+                ((a * _inputData.NominalVoltage + b * I2_more).Magnitude / a.Magnitude);
+            });
+
+            return (Xs, Ys);
+        }
+
+        #endregion
+
     }
 }
